1f7b79e6c2eea4084291d9019dcfe4e64b80530c
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-01-20 16:12:39 +0200

    create Photo Details 🤟

---
+++ ADVA/ADVA/Modules/HomeModule/PhotoDetails/View/ViewController/PhotoDetails.swift
@@ -0,0 +1,72 @@
+//
+//  PhotoDetails.swift
+//  ADVA
+//
+//  Created by iOSAYed on 20/01/2024.
+//
+
+import UIKit
+
+class PhotoDetails: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    //MARK: - Propreties
+     var photoData : PhotosResponseElement?
+    
+    
+    // MARK: - View LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupImageView()
+        // Setup tap gesture
+        setupTabGesture()
+
+    }
+
+
+    
+    
+    // MARK: - Methods
+
+    private func setupView(){
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 4.0
+
+    }
+    
+  private func setupImageView(){
+      guard let photoURL = photoData?.url else {return}
+      imageView.setImageWithLoading(url: photoURL)
+  }
+    
+    private func setupTabGesture(){
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(_:)))
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(doubleTapRecognizer)
+    }
+    
+    @objc private func handleDoubleTap(_ sender: UITapGestureRecognizer) {
+        if scrollView.zoomScale == 1 {
+            scrollView.setZoomScale(4, animated: true)
+        } else {
+            scrollView.setZoomScale(1, animated: true)
+        }
+    }
+
+
+}
+
+ extension PhotoDetails: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
+
